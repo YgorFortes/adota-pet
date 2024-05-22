81cc00b3344f542e94f67ac05f30de59ccce9db1
Ygor Fortes <ygor1888@hotmail.com>
2024-05-22 14:14:13 -0300

    create infra config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { ConsoleLogger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { ExepectionErrror } from './resource/errors/Exeception.error';
 import { GlobalLogger } from './resource/interceptor/global.logger';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfig } from './infra/config/db.config';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfig,
+      inject: [PostgresConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
