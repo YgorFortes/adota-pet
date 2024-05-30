371e043cc8c30977ff26eac4c3870f9278150047
Ygor Fortes <ygor1888@hotmail.com>
2024-05-30 20:09:47 -0300

    feat: create module from feature create user

---
+++ src/useCases/user/createUser/createUser.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/infra/db/entities/User.entity';
+import { CreateUserController } from './controller/CreateUser.controller';
+import { HashPasswordPipe } from 'src/common/pipes/HashPassword.pipe';
+import { UserRepository } from 'src/repositories/implementations/User.repository';
+import { CreateUserUseCase } from './CreateUser.useCase';
+import { RepositoryType } from 'src/enum/RepositoryType.enum';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [CreateUserController],
+  providers: [
+    HashPasswordPipe,
+    {
+      provide: RepositoryType.IUserRepository,
+      useClass: UserRepository,
+    },
+    CreateUserUseCase,
+  ],
+  exports: [CreateUserUseCase],
+})
+export class CreateUserModule {}
