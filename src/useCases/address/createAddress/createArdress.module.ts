c6181569fd21f97f256c58cea0ebf0718c5ee17b
Ygor Fortes <ygor1888@hotmail.com>
2024-06-06 16:47:41 -0300

    feat: create module for feature create address

---
+++ src/useCases/address/createAddress/createArdress.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressEntity } from 'src/infra/db/entities/Address.entity';
+import { CreateAddressUseCase } from './CreateAddress.useCase';
+import { RepositoryType } from 'src/enum/RepositoryType.enum';
+import { AddressRepository } from 'src/repositories/implementations/Address.repository';
+import { Provide } from 'src/enum/provider.enum';
+import { findAddressByCepProvider } from '../findAddressByCep/FindAddressByCep.provider';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  controllers: [],
+  providers: [
+    CreateAddressUseCase,
+    { provide: RepositoryType.IAddressRepository, useClass: AddressRepository },
+    { provide: Provide.IAddressCepFinderProvider, useClass: findAddressByCepProvider },
+  ],
+  exports: [CreateAddressUseCase],
+})
+export class CreateAddressModule {}
