bc8cddadcbde7bb5a2bb9943d8ff516a5f2c442e
Ygor Fortes <ygor1888@hotmail.com>
2024-06-06 17:14:49 -0300

    refac: create type propsGuardian and create relation with address and user

--- src/entities/Guardian.entity.ts
+++ src/entities/Guardian.entity.ts
@@ -1,6 +1,24 @@
 import { Address } from './Address.entity';
 import { User } from './User.entity';
 
+type propsGuardian = {
+  id?: string;
+
+  about?: string;
+
+  birthDate: Date;
+
+  address: Address;
+
+  user: User;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+
+  deletedAt?: Date;
+};
+
 export class Guardian {
   public readonly id: string;
 
@@ -8,11 +26,24 @@ export class Guardian {
 
   public birthDate: Date;
 
-  public user: User;
+  public readonly address: Address;
 
-  public address: Address;
+  public readonly user: User;
 
-  constructor(props: Omit<Guardian, 'id'>) {
-    Object.assign(this, props);
+  public createdAt?: Date;
+
+  public updatedAt?: Date;
+
+  public deletedAt?: Date;
+
+  constructor(props: propsGuardian) {
+    this.id = props.id;
+    this.about = props.about;
+    this.birthDate = props.birthDate;
+    this.user = props.user;
+    this.address = props.address;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+    this.deletedAt = props.deletedAt;
   }
 }
