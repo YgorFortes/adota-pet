53b3775635d05480aa5178fc53bd6eaf937d4c06
Ygor Fortes <ygor1888@hotmail.com>
2024-06-06 17:10:49 -0300

    feat: create decorator for verify field bithDate is brazilian format date

---
+++ src/common/helpers/decoratorsValidators/isBrazilianDate.decorator.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+
+import { isValid } from 'date-fns';
+import { dateFormated } from '../validation.helpers';
+
+@Injectable()
+@ValidatorConstraint({ async: false })
+export class BrazilianDateFormat implements ValidatorConstraintInterface {
+  validate(birthDate: string): boolean {
+    const birthDateFormated = dateFormated(birthDate);
+
+    if (!isValid(birthDateFormated)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  defaultMessage?(): string {
+    return 'A data de nascimento deve estar no formato DD/MM/YYYY.';
+  }
+}
+
+export const IsBrazilianDate = (optionsValidation?: ValidationOptions) => {
+  return (object: unknown, propertyName: string): void => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: optionsValidation,
+      constraints: [],
+      validator: BrazilianDateFormat,
+    });
+  };
+};
