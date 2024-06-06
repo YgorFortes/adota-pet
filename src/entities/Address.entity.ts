6ef5165fedbc195f340811f657035e9865e15721
Ygor Fortes <ygor1888@hotmail.com>
2024-06-06 16:38:50 -0300

    refac: create type propsAdress, and create propetys complement, neighborhood and deleted square

--- src/entities/Address.entity.ts
+++ src/entities/Address.entity.ts
@@ -1,5 +1,27 @@
+type propsAddress = {
+  id?: string;
+
+  city: string;
+
+  cep?: string;
+
+  state: string;
+
+  street: string;
+
+  complement?: string;
+
+  neighborhood: string;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+
+  deletedAt?: Date;
+};
+
 export class Address {
-  public readonly id: string;
+  public readonly id?: string;
 
   public city: string;
 
@@ -7,9 +29,28 @@ export class Address {
 
   public state: string;
 
-  public square: string;
+  public street: string;
 
-  constructor(props: Omit<Address, 'id'>) {
-    Object.assign(this, props);
+  public complement?: string;
+
+  public neighborhood: string;
+
+  public createdAt?: Date;
+
+  public updatedAt?: Date;
+
+  public deletedAt?: Date;
+
+  constructor(props: propsAddress) {
+    this.id = props.id;
+    this.city = props.city;
+    this.cep = props.cep;
+    this.state = props.state;
+    this.street = props.street;
+    this.neighborhood = props.neighborhood;
+    this.complement = props.complement;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+    this.deletedAt = props.deletedAt;
   }
 }
