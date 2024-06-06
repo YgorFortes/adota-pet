6136ad0a67fec95a54b3632f7b4373350a43c470
Ygor Fortes <ygor1888@hotmail.com>
2024-06-06 16:50:36 -0300

    feat: create address module

---
+++ src/useCases/address/address.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressEntity } from 'src/infra/db/entities/Address.entity';
+import { CreateAddressModule } from './createAddress/createArdress.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity]), CreateAddressModule],
+  exports: [CreateAddressModule],
+})
+export class AddressModule {}
