110e4ded55112c095be0254b132e925072556370
Ygor Fortes <ygor1888@hotmail.com>
2024-06-11 16:19:10 -0300

    feat: created new method for implemetaion guardian. method called findAllGuardians

--- src/repositories/implementations/Guardian.repostory.ts
+++ src/repositories/implementations/Guardian.repostory.ts
@@ -6,6 +6,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { ICreateGuardianUseCaseDto } from 'src/useCases/guardian/createGuardian/dtos/ICreateGuardian.UseCase.dto';
+import { IFindAllGuardiansUseCaseDto } from 'src/useCases/guardian/findAllGuardians/dtos/IFindAllGuardins.useCase.dto';
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
 
 @Injectable()
 export class GuardianRepository implements IGuardianRepository {
@@ -14,13 +16,38 @@ export class GuardianRepository implements IGuardianRepository {
     private readonly guardianRepository: Repository<GuardianEntity>,
     private readonly entityManager: EntityManager,
   ) {}
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+  async findAllGuardians(pagination: IFindAllGuardiansUseCaseDto): Promise<IPagination<Guardian>> {
+    const queryBuilder = this.guardianRepository.createQueryBuilder('guardian');
+
+    queryBuilder
+      .leftJoinAndSelect('guardian.user', 'user')
+      .leftJoinAndSelect('guardian.address', 'address')
+      .leftJoinAndSelect('guardian.pets', 'pet');
+
+    queryBuilder.skip((pagination.page - 1) * pagination.limit).take(pagination.limit);
+
+    const [guadians, guardiansCount] = await Promise.all([
+      queryBuilder.getMany(),
+      this.guardianRepository.count(),
+    ]);
+
+    const guardiansFormatted = guadians.map(guardian => {
+      const { id, about, birthDate, createdAt, updatedAt, deletedAt, address, user, pets } =
+        guardian;
+
+      return { id, user, about, birthDate, address, pets, createdAt, updatedAt, deletedAt };
+    });
+
+    const counterPage = Math.ceil(guardiansCount / pagination.limit);
+
+    return { items: guadians, meta: { counterPage, totalCount: guardiansCount } };
+  }
+
   async save(
     dataGuardian: Guardian,
     transactionalEntityManager?: EntityManager,
   ): Promise<Guardian> {
-    const { about, birthDate, address, user } = dataGuardian;
-
     const entityManager = transactionalEntityManager || this.entityManager;
     return entityManager.transaction(async transEntityManager => {
       const guardianEntity = transEntityManager.create(GuardianEntity, {
@@ -37,9 +64,5 @@ export class GuardianRepository implements IGuardianRepository {
 
       return { id, user, about, birthDate, address, createdAt, updatedAt, deletedAt };
     });
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  findByEmail(email: string): Promise<Guardian> {
-    throw new Error('Method not implemented.');
   }
 }
