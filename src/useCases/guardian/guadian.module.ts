6f36b6715a94155ac8feeb1fb50883d2985cf422
Ygor Fortes <ygor1888@hotmail.com>
2024-06-12 16:15:07 -0300

    feat: imported module for feature findGuardianById

--- src/useCases/guardian/guadian.module.ts
+++ src/useCases/guardian/guadian.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CreateGuardianModule } from './createGuardian/createGuardian.module';
 import { FindAllGuardiansModule } from './findAllGuardians/findAllGuardians.module';
+import { FindGuardianByIdModule } from './findGuardianById/findGuardianById.module';
 
 @Module({
-  imports: [CreateGuardianModule, FindAllGuardiansModule],
-  exports: [CreateGuardianModule, FindAllGuardiansModule],
+  imports: [CreateGuardianModule, FindAllGuardiansModule, FindGuardianByIdModule],
+  exports: [CreateGuardianModule, FindAllGuardiansModule, FindGuardianByIdModule],
 })
 export class GuardianModule {}
