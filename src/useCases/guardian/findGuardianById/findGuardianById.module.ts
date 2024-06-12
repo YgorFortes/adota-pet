e4db5d0323add61eb73c84a67e752d95d72c7d1f
Ygor Fortes <ygor1888@hotmail.com>
2024-06-12 16:17:33 -0300

    feat: create module for feature find guardian by id

---
+++ src/useCases/guardian/findGuardianById/findGuardianById.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GuardianEntity } from 'src/infra/db/entities/Guardian.entity';
+import { FindGuardianByIdController } from './controller/FindGuardianById.controller';
+import { RepositoryType } from 'src/enum/repositoryType.enum';
+import { GuardianRepository } from 'src/repositories/implementations/Guardian.repostory';
+import { FindGuardianByIdUseCase } from './FindGuardianById.useCase';
+import { FindUserByIdModule } from 'src/useCases/user/findUserById/findUserById.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([GuardianEntity]), FindUserByIdModule],
+  controllers: [FindGuardianByIdController],
+  providers: [
+    FindGuardianByIdUseCase,
+    { provide: RepositoryType.IGuardianRepository, useClass: GuardianRepository },
+  ],
+})
+export class FindGuardianByIdModule {}
