48539de2ac38b1fa7195f1414d3e8d45316c689c
Ygor Fortes <ygor1888@hotmail.com>
2024-06-14 15:11:09 -0300

    feat: create new method update for Address

--- src/repositories/interfaces/IAddressRepository.interface.ts
+++ src/repositories/interfaces/IAddressRepository.interface.ts
@@ -1,6 +1,8 @@
 import { Address } from 'src/entities/Address.entity';
 import { ICreateAddressDto } from 'src/useCases/address/createAddress/dtos/ICreateAddress.useCase.dto';
+import { IUpdateAddressUseCaseDto } from 'src/useCases/address/updateAddress/dtos/IUpdateAddress.useCase.dto';
 
 export interface IAddressRepository {
   save(addressDto: ICreateAddressDto): Promise<Address>;
+  updateAddress(id: string, updateAddressDto: IUpdateAddressUseCaseDto): Promise<Address>;
 }
