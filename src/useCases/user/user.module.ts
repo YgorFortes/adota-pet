fd968472f2f927c4a206f5c9f09e3cd6b762a905
Ygor Fortes <ygor1888@hotmail.com>
2024-06-18 17:35:50 -0300

    feat: imported LogoutUserModule in user.module.ts

--- src/useCases/user/user.module.ts
+++ src/useCases/user/user.module.ts
@@ -5,9 +5,15 @@ import { UserEntity } from 'src/infra/db/entities/User.entity';
 
 import { AuthenticationModule } from './autheticationUser/authentication.module';
 import { CreateUserModule } from './createUser/createUser.module';
+import { LogoutUserModule } from './logoutUser/logoutUser.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), AuthenticationModule, CreateUserModule],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthenticationModule,
+    CreateUserModule,
+    LogoutUserModule,
+  ],
   exports: [CreateUserModule, AuthenticationModule],
 })
 export class UserModule {}
