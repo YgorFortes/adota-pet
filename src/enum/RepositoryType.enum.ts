b89c5fc717fa4b843bb7eb00ca9151f0a383184c
Ygor Fortes <ygor1888@hotmail.com>
2024-06-19 18:14:33 -0300

    feat: create IShelterRepository for implementation in repositorys

--- src/enum/RepositoryType.enum.ts
+++ src/enum/RepositoryType.enum.ts
@@ -2,4 +2,5 @@ export enum RepositoryType {
   IUserRepository = 'IUserRepository',
   IGuardianRepository = 'IGuardianRepository',
   IAddressRepository = 'IAddressRepository',
+  IShelterRepository = 'IShelterRepository',
 }
