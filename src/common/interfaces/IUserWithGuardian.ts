42c12244a84873989a57d99db73486e167bfdd97
Ygor Fortes <ygor1888@hotmail.com>
2024-06-19 18:13:56 -0300

    fix: renamed IUserWithGuardian for IUserWithAssociation

--- src/common/interfaces/IUserWithGuardian.ts
+++
@@ -1,6 +0,0 @@
-import { Guardian } from 'src/entities/Guardian.entity';
-import { User } from 'src/entities/User.entity';
-
-export interface IUserWithGuardian extends User {
-  guardian?: Guardian;
-}
