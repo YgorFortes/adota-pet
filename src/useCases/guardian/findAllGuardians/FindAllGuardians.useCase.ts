48c95ae6816eda2a97410829f48284307cc05331
Ygor Fortes <ygor1888@hotmail.com>
2024-06-24 17:56:57 -0300

    refac: now throw if gurdians not founded

--- src/useCases/guardian/findAllGuardians/FindAllGuardians.useCase.ts
+++ src/useCases/guardian/findAllGuardians/FindAllGuardians.useCase.ts
@@ -1,7 +1,7 @@
-import { Inject } from '@nestjs/common';
+import { Inject, NotFoundException } from '@nestjs/common';
 import { RepositoryType } from 'src/enum/repositoryType.enum';
 import { IGuardianRepository } from 'src/repositories/interfaces/IGuardianRepository.interface';
-import { IFindAllGuardiansUseCaseDto } from './dtos/IFindAllGuardins.useCase.dto';
+import { IFindAllPaginationUseCaseDto } from '../../../common/dtos/IFindAllPagination.useCase.dto';
 import { Guardian } from 'src/entities/Guardian.entity';
 import { IPagination } from 'src/common/interfaces/IPagination.interface';
 
@@ -10,7 +10,12 @@ export class FindAllGuardiansUseCase {
     @Inject(RepositoryType.IGuardianRepository) private guardianRepository: IGuardianRepository,
   ) {}
 
-  async execute(pagination: IFindAllGuardiansUseCaseDto): Promise<IPagination<Guardian>> {
-    return this.guardianRepository.findAllGuardians(pagination);
+  async execute(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Guardian>> {
+    const guardians = await this.guardianRepository.findAllGuardians(pagination);
+    if (guardians.items.length < 1) {
+      throw new NotFoundException('Guardião não encontrado.');
+    }
+
+    return guardians;
   }
 }
