0cf309e73ab8f9d818382443b5b80f40cad4fceb
Ygor Fortes <ygor1888@hotmail.com>
2024-06-24 17:58:32 -0300

    feat: now create method findAllShelters with pagination in parameter and implemetation created in ShelterRepository

--- src/repositories/implementations/Shelter.repository.ts
+++ src/repositories/implementations/Shelter.repository.ts
@@ -6,11 +6,37 @@ import { ShelterEntity } from 'src/infra/db/entities/Shelter.entity';
 import { BaseRepository } from './BaseRepository';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
+import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
 
 @Injectable({ scope: Scope.REQUEST })
 export class ShelterRepository extends BaseRepository<ShelterEntity> implements IShelterRepository {
   constructor(dataSource: DataSource, @Inject(REQUEST) request: Request) {
     super(ShelterEntity, dataSource, request);
+  }
+
+  async findAllShelters(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Shelter>> {
+    const queryBuilder = this.repository.createQueryBuilder('shelter');
+
+    queryBuilder
+      .leftJoinAndSelect('shelter.user', 'user')
+      .leftJoinAndSelect('shelter.address', 'address')
+      .leftJoinAndSelect('shelter.pets', 'pet');
+
+    queryBuilder.skip((pagination.page - 1) * pagination.limit).take(pagination.limit);
+
+    const [shelters, sheltersCount] = await Promise.all([
+      queryBuilder.getMany(),
+      this.repository.count(),
+    ]);
+
+    const shelterFormated = shelters.map(shelter => {
+      return this.formatShelterProperties(shelter);
+    });
+
+    const counterPage = Math.ceil(sheltersCount / pagination.limit);
+
+    return { items: shelterFormated, meta: { counterPage, totalCount: sheltersCount } };
   }
 
   async save(shelterdto: Shelter): Promise<Shelter> {
