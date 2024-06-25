84f1c59c63a122ae67b623a0a5bff299dee8a473
Ygor Fortes <ygor1888@hotmail.com>
2024-06-25 16:16:29 -0300

    feat: imported module for feature find shelter by id

--- src/useCases/shelter/shelter.module.ts
+++ src/useCases/shelter/shelter.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CreateShelterModule } from './createShelter/createShelter.module';
 import { FindAllSheltersModule } from './findAllShelters/findAllShelters.module';
+import { FindShelterByIdModule } from './findShelterById/findShelterById.module';
 
 @Module({
-  imports: [CreateShelterModule, FindAllSheltersModule],
-  exports: [CreateShelterModule, FindAllSheltersModule],
+  imports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule],
+  exports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule],
 })
 export class ShelterModule {}
