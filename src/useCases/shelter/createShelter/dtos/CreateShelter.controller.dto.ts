558f5c6c659a4af8c04e81150768cfd2c8734386
Ygor Fortes <ygor1888@hotmail.com>
2024-06-25 17:57:04 -0300

    fix: renamed field abbout for about and not decorator Transform use trimString for helpers

--- src/useCases/shelter/createShelter/dtos/CreateShelter.controller.dto.ts
+++ src/useCases/shelter/createShelter/dtos/CreateShelter.controller.dto.ts
@@ -7,8 +7,8 @@ export class CreateShelterControllerDto {
   @Transform(trimString)
   @IsString({ message: 'about deve ser uma string.' })
   @MaxLength(350, { message: 'about não dever ter mais de 300 caracteres.' })
-  @IsNotEmpty({ message: 'abbout não pode ser vazio.' })
-  readonly abbout: string;
+  @IsNotEmpty({ message: 'about não pode ser vazio.' })
+  readonly about: string;
 
   @Transform(trimString)
   @IsUrl({}, { message: 'webSite deve ser uma URL válida.' })
