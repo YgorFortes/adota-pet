75218c981a2d16cbf667bb9942187039d2aaa8b9
Ygor Fortes <ygor1888@hotmail.com>
2024-06-25 17:57:35 -0300

    feat: imported module updateShelter

--- src/useCases/shelter/shelter.module.ts
+++ src/useCases/shelter/shelter.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CreateShelterModule } from './createShelter/createShelter.module';
 import { FindAllSheltersModule } from './findAllShelters/findAllShelters.module';
 import { FindShelterByIdModule } from './findShelterById/findShelterById.module';
+import { UpdateShelterModule } from './updateShelter/updateShelter.module';
 
 @Module({
-  imports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule],
-  exports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule],
+  imports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule, UpdateShelterModule],
+  exports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule, UpdateShelterModule],
 })
 export class ShelterModule {}
