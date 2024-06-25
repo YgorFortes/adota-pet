1d404951721e680e0be0ab4aabb2abcbe5ad3b5b
Ygor Fortes <ygor1888@hotmail.com>
2024-06-25 16:15:53 -0300

    chore: moved for common folder

--- src/useCases/guardian/findGuardianById/dtos/FindGuardianById.controller.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
-
-export class FindAllGuardiansControllerDto {
-  @IsUUID()
-  @IsNotEmpty({ message: 'id não deve ser vazio.' })
-  readonly id: string;
-}
