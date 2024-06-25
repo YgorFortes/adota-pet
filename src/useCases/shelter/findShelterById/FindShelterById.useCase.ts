65ad130b15ff768aedfdfc42dce102b83e1ece1f
Ygor Fortes <ygor1888@hotmail.com>
2024-06-25 16:18:50 -0300

    feat: created useCase for feature find shelter by id

---
+++ src/useCases/shelter/findShelterById/FindShelterById.useCase.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Shelter } from 'src/entities/Shelter.entity';
+import { RepositoryType } from 'src/enum/repositoryType.enum';
+import { IShelterRepository } from 'src/repositories/interfaces/IShelterRepository.interface';
+
+@Injectable()
+export class FindShelterByIdUseCase {
+  constructor(
+    @Inject(RepositoryType.IShelterRepository) private shelterRepository: IShelterRepository,
+  ) {}
+
+  async execute(shelterId: string): Promise<Shelter> {
+    const shelter = await this.shelterRepository.findShelterById(shelterId);
+
+    if (!shelter) {
+      throw new NotFoundException('Abrigo não encontrado.');
+    }
+
+    return shelter;
+  }
+}
