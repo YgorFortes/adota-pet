1d1bb80daaddd7f4f97499bd4e43c4cf2001938e
Ygor Fortes <ygor1888@hotmail.com>
2024-06-26 15:19:13 -0300

    chore: replaced useCase verifyAssociationUseCase for findUserByIdUseCase.

--- src/useCases/guardian/updateGuardian/UpdateGuardian.useCase.ts
+++ src/useCases/guardian/updateGuardian/UpdateGuardian.useCase.ts
@@ -3,29 +3,27 @@ import { RepositoryType } from 'src/enum/repositoryType.enum';
 import { IGuardianRepository } from 'src/repositories/interfaces/IGuardianRepository.interface';
 import { IUpdateGuardianUseCaseDto } from './dtos/IUpdateGuardian.useCase.dto';
 import { UpdateUserUseCase } from 'src/useCases/user/updateUser/UpdateUser.useCase';
-import { VerifyUserAssociationUseCase } from 'src/useCases/user/VerifyUserGuardian/VerifyUserAssociation.useCase';
 import { Guardian } from 'src/entities/Guardian.entity';
 import { UpdateAddressUseCase } from 'src/useCases/address/updateAddress/UpdateAddress.useCase';
-import { IUserWithAssociation } from 'src/common/interfaces/IUserWithAssociation';
 import { userAssociation } from 'src/enum/userAssociation.enum';
+import { FindUserByIdUseCase } from 'src/useCases/user/findUserById/FindUserById.useCase';
 
 @Injectable()
 export class UpdateGuardianUseCase {
   constructor(
     @Inject(RepositoryType.IGuardianRepository) private guardianRepository: IGuardianRepository,
     private updateUserUseCase: UpdateUserUseCase,
-    private verifyUserAssociationUseCase: VerifyUserAssociationUseCase,
+    private findUserByIdUseCase: FindUserByIdUseCase,
     private updateAddressUseCase: UpdateAddressUseCase,
   ) {}
 
   async execute(idUser: string, updateGuardianDto: IUpdateGuardianUseCaseDto): Promise<Guardian> {
     const { about, birthDate } = updateGuardianDto;
 
-    const userAssociationGuardian: IUserWithAssociation =
-      await this.verifyUserAssociationUseCase.findAssociantionWithUser(
-        idUser,
-        userAssociation.GUARDIAN,
-      );
+    const userAssociationGuardian = await this.findUserByIdUseCase.execute(
+      idUser,
+      userAssociation.GUARDIAN,
+    );
 
     const guardianUpdated = await this.guardianRepository.updateGuardian(
       userAssociationGuardian.guardian.id,
