af306bb70cbe9e0af624aa0e5161f443054f7013
Ygor Fortes <ygor1888@hotmail.com>
2024-06-26 15:22:22 -0300

    refac: removed method findAssociantionWithUser
    chore: renamed verifyUserAssociationWithGuardianOrShelter for validateUserAssociation

--- src/useCases/user/VerifyUserGuardian/VerifyUserAssociation.useCase.ts
+++ src/useCases/user/VerifyUserGuardian/VerifyUserAssociation.useCase.ts
@@ -1,6 +1,5 @@
 import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { IUserWithAssociation } from 'src/common/interfaces/IUserWithAssociation';
-import { User } from 'src/entities/User.entity';
 import { RepositoryType } from 'src/enum/repositoryType.enum';
 import { userAssociation } from 'src/enum/userAssociation.enum';
 import { IUserRepository } from 'src/repositories/interfaces/IUserRepository.interface';
@@ -9,12 +8,9 @@ import { IUserRepository } from 'src/repositories/interfaces/IUserRepository.int
 export class VerifyUserAssociationUseCase {
   constructor(@Inject(RepositoryType.IUserRepository) private userRepository: IUserRepository) {}
 
-  async verifyUserAssociationWithGuardianOrShelter(
-    id: string,
-    associantion: userAssociation,
-  ): Promise<void> {
-    const userAssociation: IUserWithAssociation = await this.userRepository.findUserWithAssociation(
-      id,
+  async validateUserAssociation(userId: string, associantion: userAssociation): Promise<void> {
+    const userAssociation: IUserWithAssociation = await this.userRepository.findById(
+      userId,
       associantion,
     );
 
@@ -25,12 +21,5 @@ export class VerifyUserAssociationUseCase {
     if (userAssociation.shelter) {
       throw new BadRequestException('Este usuário já está associado a um abrigo. ');
     }
-  }
-
-  async findAssociantionWithUser(id: string, associantion: userAssociation): Promise<User> {
-    const addressAndGuardianForUser: IUserWithAssociation =
-      await this.userRepository.findUserWithAssociation(id, associantion);
-
-    return addressAndGuardianForUser;
   }
 }
