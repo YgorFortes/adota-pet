e54cbb2cd718a0f6ecd821000522a9bf35794df5
Ygor Fortes <ygor1888@hotmail.com>
2024-06-26 15:20:28 -0300

    feat: imported module for feature delete shelter

--- src/useCases/shelter/shelter.module.ts
+++ src/useCases/shelter/shelter.module.ts
@@ -3,9 +3,22 @@ import { CreateShelterModule } from './createShelter/createShelter.module';
 import { FindAllSheltersModule } from './findAllShelters/findAllShelters.module';
 import { FindShelterByIdModule } from './findShelterById/findShelterById.module';
 import { UpdateShelterModule } from './updateShelter/updateShelter.module';
+import { DeleteShelterModule } from './deleteShelter/deleteShelter.module';
 
 @Module({
-  imports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule, UpdateShelterModule],
-  exports: [CreateShelterModule, FindAllSheltersModule, FindShelterByIdModule, UpdateShelterModule],
+  imports: [
+    CreateShelterModule,
+    FindAllSheltersModule,
+    FindShelterByIdModule,
+    UpdateShelterModule,
+    DeleteShelterModule,
+  ],
+  exports: [
+    CreateShelterModule,
+    FindAllSheltersModule,
+    FindShelterByIdModule,
+    UpdateShelterModule,
+    DeleteShelterModule,
+  ],
 })
 export class ShelterModule {}
