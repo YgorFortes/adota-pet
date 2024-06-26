cb2c5f8d5e2dc054c3ef0a5d5b18f7fbecd94a46
Ygor Fortes <ygor1888@hotmail.com>
2024-06-26 15:26:59 -0300

    feat: create new route /shelter DELETE

---
+++ src/useCases/shelter/deleteShelter/controller/DeleteShelter.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Delete, Request, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthenticationGuard } from 'src/common/guards/Authentication.guard';
+import { RoleUserGuard } from 'src/common/guards/RoleUser.guard';
+import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
+import { TransactionInterceptor } from 'src/resource/interceptor/transaction.interceptor';
+import { DeleteShelterUseCase } from '../DeleteShelter.useCase';
+
+@Controller('shelter')
+@UseGuards(AuthenticationGuard, RoleUserGuard)
+@UseInterceptors(TransactionInterceptor)
+export class DeleteShelterController {
+  constructor(private deleteShelterUseCase: DeleteShelterUseCase) {}
+  @Delete()
+  async handle(@Request() request: IRequestWithUser): Promise<{ message: string }> {
+    const userId = request.user.sub;
+
+    const message = await this.deleteShelterUseCase.execute(userId, request);
+
+    return { message };
+  }
+}
