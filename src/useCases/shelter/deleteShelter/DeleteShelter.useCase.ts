7fb18a95d2d6c83c41a64fb251e375d3be5acd54
Ygor Fortes <ygor1888@hotmail.com>
2024-06-26 15:28:06 -0300

    feat: create new feature where delete shelter logged using token and token is placed in list of token invalids

---
+++ src/useCases/shelter/deleteShelter/DeleteShelter.useCase.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
+import { RepositoryType } from 'src/enum/repositoryType.enum';
+import { userAssociation } from 'src/enum/userAssociation.enum';
+import { IShelterRepository } from 'src/repositories/interfaces/IShelterRepository.interface';
+import { FindUserByIdUseCase } from 'src/useCases/user/findUserById/FindUserById.useCase';
+import { LogoutUserUseCase } from 'src/useCases/user/logoutUser/LogoutUser.useCase';
+
+@Injectable()
+export class DeleteShelterUseCase {
+  constructor(
+    @Inject(RepositoryType.IShelterRepository) private shelterRepository: IShelterRepository,
+    private findUserByIdUseCase: FindUserByIdUseCase,
+    private logoutUserUseCase: LogoutUserUseCase,
+  ) {}
+
+  async execute(userId: string, request: IRequestWithUser): Promise<string> {
+    const user = await this.findUserByIdUseCase.execute(userId, userAssociation.SHELTER);
+
+    const shelterId = user.shelter.id;
+
+    const result = await this.shelterRepository.deleteShelter(shelterId);
+
+    if (!result) {
+      return `Não foi possivel deletar shelter. id: ${shelterId}`;
+    }
+
+    await this.logoutUserUseCase.execute(request);
+
+    return `Shelter id: ${shelterId} deletado com sucesso.`;
+  }
+}
