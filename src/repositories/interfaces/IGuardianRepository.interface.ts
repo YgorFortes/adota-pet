0a6d9e6378106d45eaa1b831710109f4ad988863
Ygor Fortes <ygor1888@hotmail.com>
2024-06-27 18:10:26 -0300

    refac: now tree interfaces receive in parameter update interface where photo is string, not IImageFile

--- src/repositories/interfaces/IGuardianRepository.interface.ts
+++ src/repositories/interfaces/IGuardianRepository.interface.ts
@@ -1,12 +1,19 @@
 import { IPagination } from 'src/common/interfaces/IPagination.interface';
 import { Guardian } from 'src/entities/Guardian.entity';
 import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
-import { IUpdateGuardianUseCaseDto } from 'src/useCases/guardian/updateGuardian/dtos/IUpdateGuardian.useCase.dto';
+import { IUpdateUserRepositoryDto } from './IUserRepository.interface';
+import { IUpdateAddressUseCaseDto } from 'src/useCases/address/updateAddress/dtos/IUpdateAddress.useCase.dto';
+import { GuardianCreationWithoutAddressDto } from 'src/useCases/guardian/updateGuardian/dtos/GuardianCreationWithoutAddressDto';
 
 export interface IGuardianRepository {
   save(data: Guardian): Promise<Guardian>;
   findAllGuardians(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Guardian>>;
   findGuardianById(id: string): Promise<Guardian>;
-  updateGuardian(id: string, updateGuardianDto: IUpdateGuardianUseCaseDto): Promise<Guardian>;
+  updateGuardian(id: string, updateGuardianDto: IUpdateGuardianRepositoryDto): Promise<Guardian>;
   deleteGuardian(id: string): Promise<boolean>;
+}
+
+export interface IUpdateGuardianRepositoryDto extends Partial<GuardianCreationWithoutAddressDto> {
+  user?: IUpdateUserRepositoryDto;
+  address?: IUpdateAddressUseCaseDto;
 }
--- src/repositories/interfaces/IShelterRepository.interface.ts
+++ src/repositories/interfaces/IShelterRepository.interface.ts
@@ -2,12 +2,19 @@ import { IPagination } from 'src/common/interfaces/IPagination.interface';
 import { Shelter } from 'src/entities/Shelter.entity';
 import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
 import { EntityManager } from 'typeorm';
-import { IUpdateShelterUseCaseDto } from 'src/useCases/shelter/updateShelter/dtos/IUpdateShelter.useCase.dto';
+import { ShelterCreateWithoutAddressDto } from 'src/useCases/shelter/updateShelter/dtos/ShelterCreateWithoutAddressDto';
+import { IUpdateUserRepositoryDto } from './IUserRepository.interface';
+import { IUpdateAddressUseCaseDto } from 'src/useCases/address/updateAddress/dtos/IUpdateAddress.useCase.dto';
 
 export interface IShelterRepository {
   save(shelterdto: Shelter, transaction?: EntityManager): Promise<Shelter>;
   findAllShelters(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Shelter>>;
   findShelterById(shelterId: string): Promise<Shelter>;
-  updateShelter(shelterId: string, updateShelterDto: IUpdateShelterUseCaseDto): Promise<Shelter>;
+  updateShelter(shelterId: string, updateShelterDto: IUpdateShelterRepositoryDto): Promise<Shelter>;
   deleteShelter(shelterId: string): Promise<boolean>;
+}
+
+export interface IUpdateShelterRepositoryDto extends Partial<ShelterCreateWithoutAddressDto> {
+  user?: IUpdateUserRepositoryDto;
+  address?: IUpdateAddressUseCaseDto;
 }
