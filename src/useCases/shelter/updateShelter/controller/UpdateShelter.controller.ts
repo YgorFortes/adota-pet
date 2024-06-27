898fb64eac1fcec9820398498899d74d00d374c0
Ygor Fortes <ygor1888@hotmail.com>
2024-06-27 18:11:21 -0300

    feat: create one field where receive image and validate

--- src/useCases/shelter/updateShelter/controller/UpdateShelter.controller.ts
+++ src/useCases/shelter/updateShelter/controller/UpdateShelter.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Put, Request, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Put,
+  Request,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthenticationGuard } from 'src/common/guards/Authentication.guard';
 import { RoleUserGuard } from 'src/common/guards/RoleUser.guard';
 import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
@@ -6,9 +14,12 @@ import { UpdateShelterUseCase } from '../UpdateShelter.useCase';
 import { TransactionInterceptor } from 'src/resource/interceptor/transaction.interceptor';
 import { UpdateShelterControllerDto } from '../dtos/UpdateShelter.controller.dto';
 import { Shelter } from 'src/entities/Shelter.entity';
+import { ImageValidator } from 'src/common/pipes/ImageValidator.pipe';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('shelter')
 @UseGuards(AuthenticationGuard, RoleUserGuard)
+@UseInterceptors(FileInterceptor('user.photo'))
 @UseInterceptors(TransactionInterceptor)
 export class UpdateShelterController {
   constructor(private updateShelterUseCase: UpdateShelterUseCase) {}
@@ -16,9 +27,16 @@ export class UpdateShelterController {
   async handle(
     @Body() updateShelterDto: UpdateShelterControllerDto,
     @Request() request: IRequestWithUser,
+    @UploadedFile(new ImageValidator()) photo: Express.Multer.File,
   ): Promise<{ shelter: Shelter }> {
+    const combinedData = {
+      ...updateShelterDto,
+      user: { photo: photo },
+    };
+
     const userId = request.user.sub;
-    const shelterUpdated = await this.updateShelterUseCase.execute(userId, updateShelterDto);
+
+    const shelterUpdated = await this.updateShelterUseCase.execute(userId, { ...combinedData });
 
     return { shelter: shelterUpdated };
   }
