02b9e34357468920e952e4d06cda6cebeb8d2d9b
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 16:41:43 -0300

    feat: create new feature where find pet by id and return pet

---
+++ src/useCases/pet/findPetById/FindPetById.useCase.ts
@@ -0,0 +1,17 @@
+import { Inject, NotFoundException } from '@nestjs/common';
+import { RepositoryType } from 'src/common/enum/repositoryType.enum';
+import { Pet } from 'src/entities/Pet.entity';
+import { IPetRepository } from 'src/repositories/interfaces/IPetRepository.interface';
+
+export class FindPetByIdUseCase {
+  constructor(@Inject(RepositoryType.IPetRepository) private petRepository: IPetRepository) {}
+
+  async execute(petId: string): Promise<Pet> {
+    const pet = await this.petRepository.findPetById(petId);
+
+    if (!pet) {
+      throw new NotFoundException('Pet não encontrado.');
+    }
+    return pet;
+  }
+}
