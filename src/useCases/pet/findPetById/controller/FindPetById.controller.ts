88b05acc4d86d64ec9d1732effad4ed794c3042d
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 16:41:15 -0300

    feat: create new route GET /pet:id

---
+++ src/useCases/pet/findPetById/controller/FindPetById.controller.ts
@@ -0,0 +1,16 @@
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { FindByIdControllerDto } from 'src/common/dtos/FindById.controller.dto';
+import { FindPetByIdUseCase } from '../FindPetById.useCase';
+import { Pet } from 'src/entities/Pet.entity';
+
+@Controller('pet')
+export class FindPetByIdController {
+  constructor(private findPetById: FindPetByIdUseCase) {}
+  @Get(':id')
+  // @UseInterceptors(CacheInterceptor)
+  async handle(@Param() params: FindByIdControllerDto): Promise<Pet> {
+    const pet = await this.findPetById.execute(params.id);
+    return pet;
+  }
+}
