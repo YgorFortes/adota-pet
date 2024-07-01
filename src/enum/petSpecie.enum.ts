059830ef7364eb693e86f85f262be2da3de6e2a6
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 14:44:07 -0300

    feat: create Enum for pet specie

---
+++ src/enum/petSpecie.enum.ts
@@ -0,0 +1,8 @@
+export enum PetSpecie {
+  CACHORRO = 'cachorro',
+  GATO = 'gato',
+  PASSARO = 'pássaro',
+  COELHO = 'coelho',
+  PORQUINHO_DA_INDIA = 'porquinho-da-índia',
+  OUTROS = 'outros',
+}
