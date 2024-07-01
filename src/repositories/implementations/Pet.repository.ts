d08637efd421e4638f74f1b2887151cd759a754c
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 15:46:28 -0300

    feat: implemented method findAllPets in pet.repository

--- src/repositories/implementations/Pet.repository.ts
+++ src/repositories/implementations/Pet.repository.ts
@@ -6,10 +6,26 @@ import { DataSource } from 'typeorm';
 import { Inject } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
+import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
 
 export class PetRepository extends BaseRepository<PetEntity> implements IPetRepository {
   constructor(dataSource: DataSource, @Inject(REQUEST) request: Request) {
     super(PetEntity, dataSource, request);
+  }
+
+  async findAllPets(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Pet>> {
+    const queryBuilder = this.repository.createQueryBuilder('pet');
+
+    queryBuilder.leftJoinAndSelect('pet.shelter', 'shelter');
+
+    queryBuilder.skip((pagination.page - 1) * pagination.limit).take(pagination.limit);
+
+    const [pets, petsCount] = await Promise.all([queryBuilder.getMany(), this.repository.count()]);
+
+    const counterPage = Math.ceil(petsCount / pagination.limit);
+
+    return { items: pets, meta: { counterPage, totalCount: petsCount } };
   }
 
   async savePet(petDto: Pet): Promise<Pet> {
