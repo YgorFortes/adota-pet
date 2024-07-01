3ed1fd017f0e17dbf3eaa26aec65f2439aa3cbfd
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 15:47:13 -0300

    create new method in IPetRepository.interface called findAllPets

--- src/repositories/interfaces/IPetRepository.interface.ts
+++ src/repositories/interfaces/IPetRepository.interface.ts
@@ -1,5 +1,8 @@
+import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
 import { Pet } from 'src/entities/Pet.entity';
 
 export interface IPetRepository {
   savePet(petDto: Pet): Promise<Pet>;
+  findAllPets(pagination: IFindAllPaginationUseCaseDto): Promise<IPagination<Pet>>;
 }
