2bd538961dcf346e09a687a0fc60f099ce0c8c5b
Ygor Fortes <ygor1888@hotmail.com>
2024-07-01 14:43:40 -0300

    feat: create pet entity

---
+++ src/entities/Pet.entity.ts
@@ -0,0 +1,65 @@
+import { PetSize } from 'src/enum/petSize.enum';
+import { PetStatus } from 'src/enum/petStatus.enum';
+import { Shelter } from './Shelter.entity';
+import { Guardian } from './Guardian.entity';
+import { PetSpecie } from 'src/enum/petSpecie.enum';
+
+type propsPet = {
+  id?: string;
+
+  name: string;
+
+  birthDate?: Date;
+
+  image: string;
+
+  size: PetSize;
+
+  behavior: string;
+
+  status?: PetStatus;
+
+  specie: PetSpecie;
+
+  shelter: Shelter;
+
+  guardian?: Guardian;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+
+  deletedAt?: Date;
+};
+
+export class Pet {
+  public readonly id?: string;
+  public name: string;
+  public birthDate?: Date;
+  public image: string;
+  public size: PetSize;
+  public behavior: string;
+  public status?: PetStatus;
+  public specie: PetSpecie;
+  public readonly shelter: Shelter;
+  public readonly guardian?: Guardian;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+  public deletedAt?: Date;
+
+  constructor(props: propsPet) {
+    this.id = props.id;
+    this.name = props.name;
+    this.birthDate = props.birthDate;
+    this.image = props.image;
+    this.size = props.size;
+    this.behavior = props.behavior;
+    this.status = props.status;
+    this.specie = props.specie;
+    this.shelter = props.shelter;
+    this.guardian = props.guardian;
+    this.createdAt = props.createdAt;
+    this.updatedAt = props.updatedAt;
+    this.deletedAt = props.deletedAt;
+  }
+}
