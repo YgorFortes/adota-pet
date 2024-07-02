513e4cb4d479829ca60da0c3cc88a32b7d1a21bd
Ygor Fortes <ygor1888@hotmail.com>
2024-07-02 16:23:08 -0300

    feat: export updatePet and deletePet in pet.module

--- src/useCases/pet/pet.module.ts
+++ src/useCases/pet/pet.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { CreatePetModule } from './createPet/createPet.module';
 import { FindAllPetsModule } from './findAllPets/findAllPets.module';
 import { FindPetByIdModule } from './findPetById/findPetById.module';
+import { UpdatePetModule } from './updatePet/updatePet.module';
+import { DeletePetModule } from './deletePet/deletePet.module';
 
 @Module({
-  imports: [CreatePetModule, FindAllPetsModule, FindPetByIdModule],
-  exports: [CreatePetModule, FindAllPetsModule, FindPetByIdModule],
+  imports: [
+    CreatePetModule,
+    FindAllPetsModule,
+    FindPetByIdModule,
+    UpdatePetModule,
+    DeletePetModule,
+  ],
+  exports: [
+    CreatePetModule,
+    FindAllPetsModule,
+    FindPetByIdModule,
+    UpdatePetModule,
+    DeletePetModule,
+  ],
 })
 export class PetModule {}
