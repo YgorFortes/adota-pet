519bdd769fd060d71dfd931ebd6b44cc2c0f1b71
Ygor Fortes <ygor1888@hotmail.com>
2024-07-09 16:16:06 -0300

    feat: created new entity Adoption in typeOrm

---
+++ src/infra/db/entities/Adoption.entity.ts
@@ -0,0 +1,66 @@
+import { AdoptionStatus } from '../../../common/enum/adoptionStatus.enum';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { GuardianEntity } from './Guardian.entity';
+import { PetEntity } from './Pet.entity';
+
+@Entity({ name: 'adoption' })
+export class AdoptionEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @Index()
+  id: string;
+
+  @Column({ name: 'adoption_date', nullable: false, type: 'timestamp with time zone' })
+  adoptionDate: Date;
+
+  @Column({
+    type: 'enum',
+    enum: AdoptionStatus,
+    default: AdoptionStatus.CONCLUIDO,
+  })
+  status: AdoptionStatus;
+
+  @Column({ name: 'notes', type: 'text', nullable: true })
+  notes: string;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    nullable: true,
+    default: null,
+    type: 'timestamp with time zone',
+  })
+  deletedAt: Date;
+
+  @ManyToOne(() => GuardianEntity, guardianEntity => guardianEntity.Adoptions, {
+    nullable: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'guardian_id' })
+  @Index()
+  guardian: GuardianEntity;
+
+  @ManyToOne(() => PetEntity, petEntity => petEntity.adoptions, {
+    nullable: false,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'pet_id' })
+  @Index()
+  pet: PetEntity;
+}
