3194b17a9eb8539649d8e0ba609dee3af630ea7d
Ygor Fortes <ygor1888@hotmail.com>
2024-07-11 17:22:07 -0300

    refac: now use UserRole enum instead UserAssociation
    refac: renamed provide called ISavePhoto for managePhotoInCloud

--- src/useCases/shelter/deleteShelter/DeleteShelter.useCase.ts
+++ src/useCases/shelter/deleteShelter/DeleteShelter.useCase.ts
@@ -1,10 +1,12 @@
 import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
 import { RepositoryType } from 'src/common/enum/repositoryType.enum';
-import { userAssociation } from 'src/common/enum/userAssociation.enum';
 import { IShelterRepository } from 'src/repositories/interfaces/IShelterRepository.interface';
 import { FindUserByIdUseCase } from 'src/useCases/user/findUserById/FindUserById.useCase';
 import { LogoutUserUseCase } from 'src/useCases/user/logoutUser/LogoutUser.useCase';
+import { IManagePhotoInCloudInterface } from 'src/useCases/common/ManagePhotoInCloud/interface/ISavePhotoInCloud.interface';
+import { Provide } from 'src/common/enum/provider.enum';
+import { UserRole } from 'src/common/enum/roleUser.enum';
 
 @Injectable()
 export class DeleteShelterUseCase {
@@ -12,10 +14,12 @@ export class DeleteShelterUseCase {
     @Inject(RepositoryType.IShelterRepository) private shelterRepository: IShelterRepository,
     private findUserByIdUseCase: FindUserByIdUseCase,
     private logoutUserUseCase: LogoutUserUseCase,
+    @Inject(Provide.IManagePhotoInCloudInterface)
+    private managePhotoInCloud: IManagePhotoInCloudInterface,
   ) {}
 
   async execute(userId: string, request: IRequestWithUser): Promise<boolean> {
-    const user = await this.findUserByIdUseCase.execute(userId, userAssociation.SHELTER);
+    const user = await this.findUserByIdUseCase.execute(userId, UserRole.SHELTER);
 
     const shelterId = user.shelter.id;
 
@@ -24,6 +28,8 @@ export class DeleteShelterUseCase {
     if (!result) {
       throw new InternalServerErrorException(`Não foi possivel deletar abrigo. id: ${shelterId}`);
     }
+
+    await this.managePhotoInCloud.deletePhoto(user.photo);
 
     await this.logoutUserUseCase.execute(request);
     return true;
