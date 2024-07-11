0a9ae9247f963fad71a40b3301f0e0b769845c2b
Ygor Fortes <ygor1888@hotmail.com>
2024-07-11 17:20:45 -0300

    refac: renamed provide called ISavePhoto for managePhotoInCloud
    feat: delete photo in cloud when pet is deleted

--- src/useCases/pet/deletePet/DeletePet.useCase.ts
+++ src/useCases/pet/deletePet/DeletePet.useCase.ts
@@ -1,17 +1,21 @@
 import { Inject, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Provide } from 'src/common/enum/provider.enum';
 import { RepositoryType } from 'src/common/enum/repositoryType.enum';
-import { userAssociation } from 'src/common/enum/userAssociation.enum';
+import { UserRole } from 'src/common/enum/roleUser.enum';
 import { IPetRepository } from 'src/repositories/interfaces/IPetRepository.interface';
+import { IManagePhotoInCloudInterface } from 'src/useCases/common/ManagePhotoInCloud/interface/ISavePhotoInCloud.interface';
 import { FindUserByIdUseCase } from 'src/useCases/user/findUserById/FindUserById.useCase';
 
 export class DeletePetUseCase {
   constructor(
     @Inject(RepositoryType.IPetRepository) private petRepository: IPetRepository,
     private findUserByIdUseCase: FindUserByIdUseCase,
+    @Inject(Provide.IManagePhotoInCloudInterface)
+    private managePhotoInCloud: IManagePhotoInCloudInterface,
   ) {}
 
   async execute(petId: string, userId: string): Promise<boolean> {
-    const user = await this.findUserByIdUseCase.execute(userId, userAssociation.SHELTER);
+    const user = await this.findUserByIdUseCase.execute(userId, UserRole.SHELTER);
 
     const shelterId = user.shelter.id;
 
@@ -20,6 +24,8 @@ export class DeletePetUseCase {
     if (!pet) {
       throw new NotFoundException('O pet não existe ou não está associado ao abrigo.');
     }
+
+    await this.managePhotoInCloud.deletePhoto(pet.photo);
 
     const result = await this.petRepository.deletePet(petId);
 
