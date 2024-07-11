d0a3a6748a4ecb7b5cb57baa2ddab46c99d2c973
Ygor Fortes <ygor1888@hotmail.com>
2024-07-11 18:22:26 -0300

    feat: create module for feature find pet by shelter

---
+++ src/useCases/pet/findPetByShelter/findPetByShelter.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { FindPetByShelterController } from './controller/FindPetByShelter.controller';
+import { FindPetByShelterUseCase } from './FindPetByShelter.useCase';
+import { FindUserByIdModule } from 'src/useCases/user/findUserById/findUserById.module';
+import { RepositoryType } from 'src/common/enum/repositoryType.enum';
+import { PetRepository } from 'src/repositories/implementations/Pet.repository';
+
+@Module({
+  imports: [FindUserByIdModule],
+  controllers: [FindPetByShelterController],
+  providers: [
+    FindPetByShelterUseCase,
+    {
+      provide: RepositoryType.IPetRepository,
+      useClass: PetRepository,
+    },
+  ],
+  exports: [],
+})
+export class FindPetByShelterModule {}
