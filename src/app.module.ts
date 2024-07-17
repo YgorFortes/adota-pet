3b59f732189b33c9549866353bbb5a7ee8a60108
Ygor Fortes <ygor1888@hotmail.com>
2024-07-17 16:56:52 -0300

    feat: create configuration for middlewares created in app.module and imported tokenModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
-import { ClassSerializerInterceptor, ConsoleLogger, Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ConsoleLogger,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -17,7 +24,11 @@ import { ShelterModule } from './useCases/shelter/shelter.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { PetModule } from './useCases/pet/pet.module';
 import { AdoptionModule } from './useCases/adoption/adoption.module';
-
+import { MessageModule } from './useCases/message/message.module';
+import { AuthenticationMiddleware } from './common/middlewares/Authentication.middleware';
+import { tokenModule } from './useCases/token/token.module';
+import { ShelterAuthenticationMiddleware } from './common/middlewares/ShelterAuthorization.middleware';
+import { GuardianAuthenticationMiddleware } from './common/middlewares/GuardianAuthorization.middleware';
 @Module({
   imports: [
     GuardianModule,
@@ -26,6 +37,8 @@ import { AdoptionModule } from './useCases/adoption/adoption.module';
     ShelterModule,
     PetModule,
     AdoptionModule,
+    MessageModule,
+    tokenModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -56,4 +69,47 @@ import { AdoptionModule } from './useCases/adoption/adoption.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthenticationMiddleware).forRoutes(
+      {
+        path: 'guardian',
+        method: RequestMethod.ALL,
+      },
+      {
+        path: 'logout',
+        method: RequestMethod.ALL,
+      },
+      {
+        path: 'adoption',
+        method: RequestMethod.ALL,
+      },
+      { path: 'pet', method: RequestMethod.POST },
+      { path: 'pet', method: RequestMethod.PUT },
+      { path: 'pet', method: RequestMethod.DELETE },
+      { path: 'shelter', method: RequestMethod.ALL },
+    );
+
+    consumer
+      .apply(ShelterAuthenticationMiddleware)
+      .forRoutes(
+        { path: 'shelter', method: RequestMethod.DELETE },
+        { path: 'shelter', method: RequestMethod.POST },
+        { path: 'shelter', method: RequestMethod.PUT },
+        { path: 'pet', method: RequestMethod.POST },
+        { path: 'pet', method: RequestMethod.PUT },
+        { path: 'pet', method: RequestMethod.DELETE },
+        { path: 'adoption', method: RequestMethod.POST },
+        { path: 'adoption', method: RequestMethod.DELETE },
+        { path: 'guardian', method: RequestMethod.GET },
+      );
+
+    consumer
+      .apply(GuardianAuthenticationMiddleware)
+      .forRoutes(
+        { path: 'guardian', method: RequestMethod.POST },
+        { path: 'guardian', method: RequestMethod.DELETE },
+        { path: 'guardian', method: RequestMethod.PUT },
+      );
+  }
+}
