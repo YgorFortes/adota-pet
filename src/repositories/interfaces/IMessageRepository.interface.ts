42b39914db95e579560d956a6086a08d25be67a5
Ygor Fortes <ygor1888@hotmail.com>
2024-07-19 15:41:42 -0300

    feat: created method for interface called findMessagesByShelter

--- src/repositories/interfaces/IMessageRepository.interface.ts
+++ src/repositories/interfaces/IMessageRepository.interface.ts
@@ -1,13 +1,22 @@
+import { IFindAllPaginationUseCaseDto } from 'src/common/dtos/IFindAllPagination.useCase.dto';
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
 import { Message } from 'src/entities/Message.entity';
 
 export interface IMessageRepository {
   saveMessage(message: Message): Promise<Message>;
+
   findMessage(messageId: string): Promise<Message>;
+
   findAllMessagesByPet(
     shelterId: string,
     guardianId: string,
     petId: string,
   ): Promise<Array<Message>>;
+
+  findMessagesByShelter(
+    pagination: IFindAllPaginationUseCaseDto,
+    shelterId: string,
+  ): Promise<IPagination<Message>>;
 
   findMessagesByGuardian(guardianId: string): Promise<Array<Message>>;
 }
