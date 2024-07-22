7b7cfaa9742f96200863cf0d466a130cc9747f50
Ygor Fortes <ygor1888@hotmail.com>
2024-07-22 16:52:38 -0300

    chore: name for class changed findMessageByGuardian for findMessagesByGuardian

--- src/useCases/message/message.module.ts
+++ src/useCases/message/message.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { CreateMessageModule } from './createMessage/createMessage.module';
 import { FindMessagesByShelterModule } from './findMessagesByShelter/FindMessagesByShelter.module';
 import { FindMessageByIdModule } from './findMessageById/findMessageShelterById.module';
-import { FindMessageByGuardianModule } from './findMessageByGuardian/findMessageByGuardian.module';
+import { FindMessageByGuardianModule } from './findMessageByGuardian/findMessagesByGuardian.module';
 
 @Module({
   imports: [
