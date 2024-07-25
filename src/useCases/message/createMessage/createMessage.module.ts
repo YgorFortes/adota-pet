6aa7cdaf1aa15c47a3e4f68f150d830c09039f2d
Ygor Fortes <ygor1888@hotmail.com>
2024-07-25 14:36:12 -0300

    refac: now exist controller for guardian and shelter

--- src/useCases/message/createMessage/createMessage.module.ts
+++ src/useCases/message/createMessage/createMessage.module.ts
@@ -4,14 +4,16 @@ import { RepositoryType } from 'src/common/enum/repositoryType.enum';
 import { MessageRepository } from 'src/repositories/implementations/Message.repository';
 import { FindShelterByIdModule } from 'src/useCases/shelter/findShelterById/findShelterById.module';
 import { FindPetByIdModule } from 'src/useCases/pet/findPetById/findPetById.module';
-import { CreateMessageController } from './controller/CreateMessage.controller';
+import { CreateGuardianMessageController } from './controller/CreateGuardianMessage.controller';
 import { FindUserByIdModule } from 'src/useCases/user/findUserById/findUserById.module';
 import { Provide } from 'src/common/enum/provider.enum';
 import { ManageEmailProvide } from '../sendEmail/ManageEmail.provide';
+import { FindGuardianByIdModule } from 'src/useCases/guardian/findGuardianById/findGuardianById.module';
+import { CreateShelterMessageController } from './controller/CreateShelterMessage.controller';
 
 @Module({
-  imports: [FindUserByIdModule, FindShelterByIdModule, FindPetByIdModule],
-  controllers: [CreateMessageController],
+  imports: [FindUserByIdModule, FindShelterByIdModule, FindPetByIdModule, FindGuardianByIdModule],
+  controllers: [CreateGuardianMessageController, CreateShelterMessageController],
   providers: [
     CreateMessageUseCase,
     { provide: RepositoryType.IMessageRepository, useClass: MessageRepository },
