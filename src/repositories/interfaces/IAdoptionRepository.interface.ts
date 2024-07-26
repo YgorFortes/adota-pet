a4af92800df266eec6b9bee79ca4bf71efc65663
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 14:36:38 -0300

    feat: created method called findAllAdoption

--- src/repositories/interfaces/IAdoptionRepository.interface.ts
+++ src/repositories/interfaces/IAdoptionRepository.interface.ts
@@ -1,9 +1,15 @@
+import { IPagination } from 'src/common/interfaces/IPagination.interface';
 import { Adoption } from 'src/entities/Adoption.entity';
+import { IFilterFindAllAdoptionUseCaseDto } from 'src/useCases/adoption/findAllAdoption/dto/FiltersFindAllAdoption.useCase.dto';
 
 export type AdoptionWithoutGuardianAndPet = Omit<Adoption, 'guardian' | 'pet'>;
 
 export interface IAdoptionRepository {
   saveAdoption(adoptionDto: Adoption): Promise<AdoptionWithoutGuardianAndPet>;
+  findAllAdoption(
+    filters: IFilterFindAllAdoptionUseCaseDto,
+    shelterId: string,
+  ): Promise<IPagination<Adoption>>;
   findAdoptionById(adoptionId: string, userId?: string): Promise<Adoption>;
   deleteAdoption(adoptionId: string): Promise<boolean>;
 }
