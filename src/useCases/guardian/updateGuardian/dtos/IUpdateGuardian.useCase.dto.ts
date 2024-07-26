0e35c45a89c3808378d883176d53491f0117fd05
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:49:55 -0300

    refac: IUpdateGuardianUseCaseDto is partial ICreateGuardianUseCaseDto

--- src/useCases/guardian/updateGuardian/dtos/IUpdateGuardian.useCase.dto.ts
+++ src/useCases/guardian/updateGuardian/dtos/IUpdateGuardian.useCase.dto.ts
@@ -1,8 +1,8 @@
 import { IUpdateUserUseCaseDto } from 'src/useCases/user/updateUser/dtos/IUpdateUser.useCase.dto';
 import { IUpdateAddressUseCaseDto } from 'src/useCases/address/updateAddress/dtos/IUpdateAddress.useCase.dto';
-import { GuardianCreationWithoutAddressDto } from './GuardianCreationWithoutAddressDto';
+import { ICreateGuardianUseCaseDto } from '../../createGuardian/dtos/ICreateGuardian.UseCase.dto';
 
-export interface IUpdateGuardianUseCaseDto extends Partial<GuardianCreationWithoutAddressDto> {
+export interface IUpdateGuardianUseCaseDto extends Partial<ICreateGuardianUseCaseDto> {
   user?: IUpdateUserUseCaseDto;
   address?: IUpdateAddressUseCaseDto;
 }
