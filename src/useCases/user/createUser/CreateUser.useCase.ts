4de3dd49fa79e5d4a33a75a4d3dd94b84f6950e8
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:58:05 -0300

    refac: now use save user, address, guardian or shelter.
    refac: retired routes POST /shelter / guardian
    
    refac: keep  the scope of saving users and their roles in a route

--- src/useCases/user/createUser/CreateUser.useCase.ts
+++ src/useCases/user/createUser/CreateUser.useCase.ts
@@ -7,18 +7,25 @@ import { Provide } from 'src/common/enum/provider.enum';
 import { IPayLoad } from 'src/common/interfaces/IPayLoad.interface';
 import { JwtService } from '@nestjs/jwt';
 import { IManagePhotoInCloudInterface } from 'src/useCases/common/ManagePhotoInCloud/interface/ISavePhotoInCloud.interface';
+import { CreateAddressUseCase } from 'src/useCases/address/createAddress/CreateAddress.useCase';
+import { CreateGuardianUseCase } from 'src/useCases/guardian/createGuardian/CreateGuardian.userCase';
+import { CreateShelterUseCase } from 'src/useCases/shelter/createShelter/CreateShelter.useCase';
+import { UserRole } from 'src/common/enum/roleUser.enum';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(
     @Inject(RepositoryType.IUserRepository) private userRepository: IUserRepository,
+    private createAddressUseCase: CreateAddressUseCase,
+    private createGuardianUseCase: CreateGuardianUseCase,
+    private createShelterUseCase: CreateShelterUseCase,
     @Inject(Provide.IManagePhotoInCloudInterface)
     private managePhotoInCloud: IManagePhotoInCloudInterface,
     private readonly jtwService: JwtService,
   ) {}
 
   async execute(dtoUser: ICreateUserUseCaseDTO): Promise<{ user: User; token: string }> {
-    const { photo } = dtoUser;
+    const { photo, guardian, shelter, address } = dtoUser;
 
     const userExist = await this.userRepository.findByEmail(dtoUser.email);
 
@@ -36,6 +43,16 @@ export class CreateUserUseCase {
       sub: userSaved.id,
       nameUser: userSaved.name,
     };
+
+    const addressCreated = await this.createAddressUseCase.execute(address);
+
+    if (guardian && userSaved.role === UserRole.GUARDIAN) {
+      await this.createGuardianUseCase.execute(guardian, userSaved, addressCreated);
+    }
+
+    if (shelter && userSaved.role === UserRole.SHELTER) {
+      await this.createShelterUseCase.execute(shelter, userSaved, addressCreated);
+    }
 
     return { user: userSaved, token: await this.jtwService.signAsync(payload) };
   }
