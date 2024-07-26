559a2b17a7ed7df475855f0439933dd394c6dcbb
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:46:49 -0300

    refac: now guardian Use case saved in database only guardian

--- src/useCases/guardian/createGuardian/CreateGuardian.userCase.ts
+++ src/useCases/guardian/createGuardian/CreateGuardian.userCase.ts
@@ -2,34 +2,27 @@ import { RepositoryType } from 'src/common/enum/repositoryType.enum';
 import { IGuardianRepository } from 'src/repositories/interfaces/IGuardianRepository.interface';
 import { ICreateGuardianUseCaseDto } from './dtos/ICreateGuardian.UseCase.dto';
 import { Guardian } from 'src/entities/Guardian.entity';
-import { CreateAddressUseCase } from 'src/useCases/address/createAddress/CreateAddress.useCase';
 import { Inject, Injectable } from '@nestjs/common';
-import { VerifyUserAssociationUseCase } from 'src/useCases/user/VerifyUserGuardian/VerifyUserAssociation.useCase';
-import { FindUserByIdUseCase } from 'src/useCases/user/findUserById/FindUserById.useCase';
-import { UserRole } from 'src/common/enum/roleUser.enum';
+import { User } from 'src/entities/User.entity';
+import { Address } from 'src/entities/Address.entity';
 
 @Injectable()
 export class CreateGuardianUseCase {
   constructor(
     @Inject(RepositoryType.IGuardianRepository) private guardianRepository: IGuardianRepository,
-    private createAddressUseCase: CreateAddressUseCase,
-    private verifyUserGuardianUseCase: VerifyUserAssociationUseCase,
-    private findUserByIdUseCase: FindUserByIdUseCase,
   ) {}
 
-  async execute(guardianDto: ICreateGuardianUseCaseDto): Promise<Guardian> {
-    const { about, birthDate, address, idUser } = guardianDto;
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const user = await this.findUserByIdUseCase.execute(idUser, UserRole.GUARDIAN);
-    await this.verifyUserGuardianUseCase.validateUserAssociation(idUser, UserRole.GUARDIAN, user);
-
-    const addressCreated = await this.createAddressUseCase.execute(address);
+  async execute(
+    guardianDto: ICreateGuardianUseCaseDto,
+    user: User,
+    address: Address,
+  ): Promise<Guardian> {
+    const { about, birthDate } = guardianDto;
 
     const guardian = new Guardian({
       about,
       birthDate,
-      address: addressCreated,
+      address: address,
       user,
     });
 
