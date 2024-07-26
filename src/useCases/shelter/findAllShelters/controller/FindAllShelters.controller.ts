b027ade85cda87ade3553c56fc5865476d6bae46
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 14:44:06 -0300

    refac: FindAllSheltersController received filters in parameter

--- src/useCases/shelter/findAllShelters/controller/FindAllShelters.controller.ts
+++ src/useCases/shelter/findAllShelters/controller/FindAllShelters.controller.ts
@@ -1,17 +1,17 @@
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { Shelter } from 'src/entities/Shelter.entity';
-import { FindAllPaginationControllerDto } from 'src/common/dtos/FindAllPagination.controller.dto';
 import { FindAllSheltersUseCase } from '../FindAllShelters.useCase';
 import { IPagination } from 'src/common/interfaces/IPagination.interface';
+import { FiltersFindAllSheltersDto } from '../dto/FiltersFindAllShelters.controller.dto';
 
 @Controller('shelter')
 export class FindAllSheltersController {
   constructor(private readonly findAllShelterUseCase: FindAllSheltersUseCase) {}
   @Get()
   @UseInterceptors(CacheInterceptor)
-  async handle(@Query() pagination: FindAllPaginationControllerDto): Promise<IPagination<Shelter>> {
-    const allShelters = await this.findAllShelterUseCase.execute(pagination);
+  async handle(@Query() filters: FiltersFindAllSheltersDto): Promise<IPagination<Shelter>> {
+    const allShelters = await this.findAllShelterUseCase.execute(filters);
     return allShelters;
   }
 }
