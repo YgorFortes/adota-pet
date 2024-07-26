f6e74f2a2ffea194129ca14dc78ec80c26a794bd
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:51:36 -0300

    fix: now DeletePetController received TransactionInterceptor

--- src/useCases/pet/deletePet/controller/DeletePet.controller.ts
+++ src/useCases/pet/deletePet/controller/DeletePet.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Delete, Param, Request } from '@nestjs/common';
+import { Controller, Delete, Param, Request, UseInterceptors } from '@nestjs/common';
 import { DeletePetUseCase } from '../DeletePet.useCase';
 import { IdParamControllerDto } from 'src/common/dtos/IdParam.controller.dto';
 import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
+import { TransactionInterceptor } from 'src/resource/interceptor/transaction.interceptor';
 
 @Controller('pet')
+@UseInterceptors(TransactionInterceptor)
 export class DeletePetController {
   constructor(private deletePetUseCase: DeletePetUseCase) {}
 
