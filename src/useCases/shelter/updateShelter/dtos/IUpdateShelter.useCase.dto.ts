441c1c0d7e211db13387b4452317d51a2f0a3798
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:54:56 -0300

    refac: IUpdateShelterUseCaseDto is partial ICreateShelterUseCaseDto

--- src/useCases/shelter/updateShelter/dtos/IUpdateShelter.useCase.dto.ts
+++ src/useCases/shelter/updateShelter/dtos/IUpdateShelter.useCase.dto.ts
@@ -1,8 +1,8 @@
 import { IUpdateAddressUseCaseDto } from 'src/useCases/address/updateAddress/dtos/IUpdateAddress.useCase.dto';
-import { ShelterCreateWithoutAddressDto } from './ShelterCreateWithoutAddressDto';
 import { IUpdateUserUseCaseDto } from 'src/useCases/user/updateUser/dtos/IUpdateUser.useCase.dto';
+import { ICreateShelterUseCaseDto } from '../../createShelter/dtos/ICreateShelter.UseCase.dto';
 
-export interface IUpdateShelterUseCaseDto extends Partial<ShelterCreateWithoutAddressDto> {
+export interface IUpdateShelterUseCaseDto extends Partial<ICreateShelterUseCaseDto> {
   user?: IUpdateUserUseCaseDto;
   address?: IUpdateAddressUseCaseDto;
 }
