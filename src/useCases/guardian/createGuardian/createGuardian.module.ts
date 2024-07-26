3d8870eb7164ba44450329919d4ef4c3eda805b9
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 17:47:40 -0300

    refac: deletead CreateGuadianController

--- src/useCases/guardian/createGuardian/createGuardian.module.ts
+++ src/useCases/guardian/createGuardian/createGuardian.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GuardianRepository } from 'src/repositories/implementations/Guardian.repostory';
 import { CreateGuardianUseCase } from 'src/useCases/guardian/createGuardian/CreateGuardian.userCase';
-
-import { CreateGuadianController } from './controller/CreateGuardian.controller';
 import { RepositoryType } from 'src/common/enum/repositoryType.enum';
-
 import { CreateAddressModule } from 'src/useCases/address/createAddress/createArdress.module';
 import { VerifyUserAssociationModule } from 'src/useCases/user/VerifyUserGuardian/verifyUserAssociation.module';
 import { FindUserByIdModule } from 'src/useCases/user/findUserById/findUserById.module';
 
 @Module({
   imports: [VerifyUserAssociationModule, CreateAddressModule, FindUserByIdModule],
-  controllers: [CreateGuadianController],
+  controllers: [],
   providers: [
     CreateGuardianUseCase,
     { provide: RepositoryType.IGuardianRepository, useClass: GuardianRepository },
