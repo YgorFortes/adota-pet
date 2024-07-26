c264f5d9608789e572d2df06322754b360d54362
Ygor Fortes <ygor1888@hotmail.com>
2024-07-26 14:40:14 -0300

    refac: FindMessageShelterByIdController now use CacheInterceptor

--- src/useCases/message/findMessageById/controller/FindMessageShelterById.controller.ts
+++ src/useCases/message/findMessageById/controller/FindMessageShelterById.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Param, Request } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseInterceptors } from '@nestjs/common';
 import { IdParamControllerDto } from 'src/common/dtos/IdParam.controller.dto';
 import { IRequestWithUser } from 'src/common/interfaces/IRequestWithUser.interface';
 import { Message } from 'src/entities/Message.entity';
 import { FindMessageByIdUserCase } from '../FindMessageShelterById.useCase';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('shelter')
 export class FindMessageShelterByIdController {
   constructor(private findMessageByIdUserCase: FindMessageByIdUserCase) {}
 
   @Get('message/:id')
+  @UseInterceptors(CacheInterceptor)
   async handle(
     @Param() params: IdParamControllerDto,
     @Request() request: IRequestWithUser,
